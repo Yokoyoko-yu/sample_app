fadd6a2fd51eef8fb677b13861520c7f8f97efd6
Yokoyoko <yudai20022002@icloud.com>
2024-03-18 17:01:04 +0900

    Add user following

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
     has_many :microposts,dependent: :destroy
     attr_accessor :remember_token, :activation_token,:reset_token
+    has_many :active_relationships,class_name:"Relationship",foreign_key:"follower_id",dependent: :destroy
     before_save   :downcase_email
     before_create :create_activation_digest
     validates :name,presence: true,length:{maximum:50}
@@ -8,6 +9,11 @@ class User < ApplicationRecord
     validates :email,presence: true,length:{maximum:255},format:{with:VALID_EMAIL_REGEX},uniqueness:true
     has_secure_password
     validates :password,presence: true,length:{minimum:6},allow_nil: true
+    has_many :following, through: :active_relationships, source: :followed
+    has_many :passive_relationships, class_name:  "Relationship",
+                                   foreign_key: "followed_id",
+                                   dependent:   :destroy
+    has_many :followers, through: :passive_relationships, source: :follower
 
     def User.digest(string)
         cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
@@ -69,7 +75,22 @@ class User < ApplicationRecord
     end
 
     def feed 
-        Micropost.where("user_id=?",id)
+        following_ids = "SELECT followed_id FROM relationships
+        WHERE  follower_id = :user_id"
+        Micropost.where("user_id IN (#{following_ids}) OR user_id = :user_id", user_id: id)
+        .includes(:user,image_attachment: :blob)
+    end
+
+    def follow(other_user)
+        following << other_user unless self==other_user
+    end
+
+    def unfollow(other_user)
+        following.delete(other_user)
+    end
+
+    def following?(other_user)
+        following.include?(other_user)
     end
 
     private
